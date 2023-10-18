e1479599b8986ea9681b6de1103a4b1f379c17f9
Polojames552 <Polojames552@gmail.com>
2023-10-19 00:09:39 +0800

    Add Products and verify user

--- src/PHP/Authentication/Register.php
+++ src/PHP/Authentication/Register.php
@@ -1,11 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "id21391162_secretkrich";
-$password = "@Mysecret_db21";
+$db_username = "id21391162_secretkrich";
+$db_password = "@Mysecret_db21";
 $dbname = "id21391162_secretkrich_db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
@@ -21,14 +21,15 @@ $lname = $data->Lname;
 $email = $data->Email;
 $address = $data->Address;
 $number = $data->Number;
-$username = $data->Username;
+$user = $data->Username; // Changed from $username to $user to avoid conflict
 $password = $data->Password;
 $picture = $data->Picture;
+$currentTime = date("Y-m-d H:i:s");
 
 // Using prepared statements to prevent SQL injection
-$sql = $conn->prepare("INSERT INTO users_tbl (Fname, Lname, Email, Address, Number, Username, Password, Picture, Status, Role) 
-                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'Unverified', 'User')");
-$sql->bind_param("ssssssss", $fname, $lname, $email, $address, $number, $username, $password, $picture);
+$sql = $conn->prepare("INSERT INTO users_tbl (Fname, Lname, Email, Address, Number, Username, Password, Picture, Status, Role, time_updated) 
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'Unverified', 'User', ?)");
+$sql->bind_param("sssssssss", $fname, $lname, $email, $address, $number, $user, $password, $picture, $currentTime);
 
 if ($sql->execute()) {
     $response = array('success' => true, 'message' => 'Registration Successful');
